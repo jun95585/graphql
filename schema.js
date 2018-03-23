f20e32447c23a105354d0c1700a582c01665907a
junge.wang <930114164@qq.com>
2018-03-23 18:55:16 +0800

    graphql

---
+++ schema.js
@@ -0,0 +1,24 @@
+import {
+    GraphQLObjectType,
+    GraphQLSchema,
+    GraphQLInt
+ } from "graphql";
+
+ let count = 0;
+
+ let schema = new GraphQLSchema({
+     query: new GraphQLObjectType ({
+        name: 'RootQueryType',
+        fields: {
+            count: {
+                type: GraphQLInt,
+                description: 'The count!',
+                resolve: function () {
+                    return count;
+                }
+            }
+        }
+     }) 
+ })
+
+export default schema;
